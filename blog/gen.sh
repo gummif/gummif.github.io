07a803a83d1037a29ec1deb1f862586fbbda3498
Gudmundur Adalsteinsson <ofpgummi@yahoo.com>
2019-06-05 21:15:57 +0000

    Minor changes

--- blog/gen.sh
+++ blog/gen.sh
@@ -2,6 +2,6 @@
 
 name="Gudmundur's Blog"
 
-pandoc -s -o index.html --metadata pagetitle="$name" -c style.css index.md
+pandoc -s -o index.html --metadata pagetitle="$name" -c style.css _index.md
 pandoc -s -o contract_code_gen.html --metadata pagetitle="$name -- Contract code gen" -c style.css contract_code_gen.md
 
